2791131c0b8dd0041cfb4b787310b0165ce86236
admin <admin@HOMY>
2019-09-16 23:22:09 +0500

    Добавьте файлы проекта.

---
+++ ConsoleTCPServer/Program.cs
@@ -0,0 +1,28 @@
+﻿ using System;
+ using System.Collections.Generic;
+ using System.Text;
+ using System.Net.Sockets;
+ using System.Net;
+ using System.Threading;
+ using System.Text.RegularExpressions;
+ using System.IO;
+
+namespace ConsoleTCPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Определим нужное максимальное количество потоков
+            // Пусть будет по 4 на каждый процессор
+            int MaxThreadsCount = Environment.ProcessorCount * 4;
+            // Установим максимальное количество рабочих потоков
+            ThreadPool.SetMaxThreads(MaxThreadsCount, MaxThreadsCount);
+            // Установим минимальное количество рабочих потоков
+            ThreadPool.SetMinThreads(2, 2);
+            // Создадим новый сервер на порту 80
+            new Server(808);
+        }
+    }
+}
+ 
---
+++ ConsoleTCPServer/TCPServer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace ConsoleTCPServer
+{
+    class Server
+    {
+        TcpListener Listener; // Объект, принимающий TCP-клиентов
+                              // Запуск сервера
+        public Server(int Port)
+        {
+            Listener = new TcpListener(IPAddress.Any, Port); // Создаем "слушателя" для указанного порта
+            Listener.Start(); // Запускаем его
+                              // В бесконечном цикле
+            while (true)
+            {
+                // Принимаем новых клиентов. После того, как клиент был принят, он передается в новый поток (ClientThread)
+                // с использованием пула потоков.
+                ThreadPool.QueueUserWorkItem(new WaitCallback(ClientThread), Listener.AcceptTcpClient());
+            }
+        }
+        static void ClientThread(Object StateInfo)
+        {
+            // Просто создаем новый экземпляр класса Client и передаем ему приведенный к классу TcpClient объект StateInfo
+            new Client((TcpClient)StateInfo);
+        }
+        // Остановка сервера
+        ~Server()
+        {
+            // Если "слушатель" был создан
+            if (Listener != null)
+            {
+                // Остановим его
+                Listener.Stop();
+            }
+        }
+    }
+}
+
